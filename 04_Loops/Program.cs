53f73d948d9d7d79c8fff68a076c50640dfe904d
Nazmiyee1102 <159723558+Nazmiyee1102@users.noreply.github.com>
2024-10-09 01:38:00 +0300

    Ders 5-6

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü                 
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.Write($"{i} -");
+            //}
+
+            //for (int i = 0; i <= 50; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Ekrana kaç defa yazdırmak istersiniz: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < finishValue; i++)
+            //{
+            //    Console.WriteLine("Dördüncü ders.");
+            //}
+
+            #endregion
+
+            #region For Döngüsü ile Karar Yapıları                              
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    totalValue += i;
+            //}
+
+            //Console.WriteLine(totalValue);
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.WriteLine(totalValue);
+
+            int count = 0;
+            //for (int i = 1; i < 50; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine(count);
+
+            //int bacterium = 1;
+
+            //for (int i = 1; i <= 24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine($"{i}. saat sonunda: {bacterium}");
+            //}
+
+
+
+            #endregion
+
+            #region While Döngüsü   
+
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+
+            //    Console.WriteLine("Merhaba");
+            //    i++;
+            //}
+
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+
+            //    }
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+
+            //while (i <= 10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+
+            //Console.WriteLine(sum);
+
+            //klavyeden girilen 3 basamaklı sayının basamakları toplamı
+            //Console.Write("Sayıyı giriniz: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int ones, tens, hundreds, sum;
+
+            //ones = number % 10;
+            //hundreds = number / 100;
+            //tens = (number % 100) / 10;
+
+            //Console.WriteLine($"{ones} - {tens} - {hundreds}");
+            //sum = ones + tens + hundreds; 
+            //Console.WriteLine(sum);
+
+
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
